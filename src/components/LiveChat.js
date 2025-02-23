3b621f5b95f70ebbad7df3de24128305f42cc10a
TanCodeDeV <bhavetanu@gmail.com>
2025-02-23 16:36:17 +0530

    added user Live chat

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,11 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ChatMessage from "./ChatMessage";
 import { useDispatch, useSelector } from "react-redux";
 import store from "../utils/Store";
 import { addMessage } from "../utils/chatSlice";
 import { randomNames, randomComments } from "../utils/helper";
+import { USERLOGOURL } from "../utils/Constants";
 
 const LiveChat = () => {
+  const [liveMessage, setLiveMessage] = useState();
   const chatMessage = useSelector((store) => store.chat.messages);
   const dispatch = useDispatch();
   useEffect(() => {
@@ -17,17 +19,46 @@ const LiveChat = () => {
           text: randomComments(),
         })
       );
-    }, 2000);
+    }, 1000);
 
     return () => {
       clearInterval(timer);
     };
   }, []);
   return (
-    <div className="w-full flex flex-col-reverse border h-[524px] border-black rounded bg-gray-100 overflow-scroll">
-      {chatMessage.map((message, index) => (
-        <ChatMessage key={index} name={message.name} text={message.text} />
-      ))}
+    <div>
+      <div className="w-full flex flex-col-reverse h-[524px] border  border-gray-500 rounded bg-gray-100 overflow-scroll">
+        {chatMessage.map((message, index) => (
+          <ChatMessage key={index} name={message.name} text={message.text} />
+        ))}
+      </div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch(
+            addMessage({
+              name: "Tanmayi Bhave",
+              text: liveMessage,
+            })
+          );
+        }}
+        className="w-full grid grid-cols-12 border border-gray-500 rounded bg-gray-100 border-yt-transparent"
+      >
+        <img
+          className="h-[50px] w-auto p-2 col-span-1"
+          src={USERLOGOURL}
+          alt="user-icon"
+        ></img>
+        <input
+          className="col-span-10 p-1 m-1 border border-black rounded-xl bg-gray-100"
+          type="text"
+          value={liveMessage}
+          onChange={(e) => {
+            setLiveMessage(e.target.value);
+          }}
+        ></input>
+        <button className="col-span-1">▶️</button>
+      </form>
     </div>
   );
 };
--- src/utils/chatSlice.js
+++ src/utils/chatSlice.js
@@ -7,6 +7,7 @@ const chatSlice = createSlice({
   },
   reducers: {
     addMessage: (state, action) => {
+      state.messages.splice(10, 5);
       state.messages.unshift(action.payload);
     },
   },
